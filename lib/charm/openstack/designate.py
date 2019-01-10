e2dae85ef999bb5481ffaf4daad7558ad8f17298
Salman Khan <salman.khan@yoti.com>
2019-01-10 12:59:37 +0000

    Removed memcached dependency from the charm for coordination.
    There is still discussion going on in charms community not to use the
    memcached as a backend coordinator because it is not recommended by
    OpenStack and the designate coordination code requires heartbeats,
    locks, etc. which is not supported by memcached. So there will probably
    be more changes coming in the charm to either use redis, etcd or zookeeper
    for coordination backend.
    
    Refrence: https://bugs.launchpad.net/charm-designate/+bug/1759597

--- lib/charm/openstack/designate.py
+++ lib/charm/openstack/designate.py
@@ -291,7 +291,6 @@ class DesignateAdapters(openstack_adapters.OpenStackAPIRelationAdapters):
         'shared_db': DesignateDBAdapter,
         'cluster': openstack_adapters.PeerHARelationAdapter,
         'dns_backend': BindRNDCRelationAdapter,
-        'coordinator_memcached': openstack_adapters.MemcacheRelationAdapter,
     }
 
 
@@ -317,8 +316,7 @@ class DesignateCharm(openstack_charm.HAOpenStackCharm):
         }
     }
 
-    required_relations = ['shared-db', 'amqp', 'identity-service',
-                          'coordinator-memcached']
+    required_relations = ['shared-db', 'amqp', 'identity-service']
 
     restart_map = {
         '/etc/default/openstack': services,
